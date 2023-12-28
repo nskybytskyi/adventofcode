1f3000b393fba2b54aa833321b337a7cb451e6de
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-12-28 16:57:10 +0200

    chore(refactor): 2023/15

--- 2023/15/main.py
+++ 2023/15/main.py
@@ -15,32 +15,37 @@ def read_and_parse(filename: str) -> list[str]:
 
 
 def solve_part_one(instructions: list[str]) -> int:
-    return sum(custom_hash(instruction) for instruction in instructions)
+    return sum(map(custom_hash, instructions))
+
+
+def calculate_power(boxes: dict[int, dict[str, int]]) -> int:
+    return sum(
+        (box_index + 1) * (lens_index + 1) * focal_length
+        for box_index, box in boxes.items()
+        for lens_index, focal_length in enumerate(box.values())
+    )
+
+
+REMOVE_REGEX = re.compile("([a-z]+)-")
+ASSIGN_REGEX = re.compile("([a-z]+)=([0-9])")
 
 
 def solve_part_two(instructions: list[str]) -> int:
     boxes = collections.defaultdict(dict)
 
-    remove = re.compile("([a-z]+)-")
-    assign = re.compile("([a-z]+)=([0-9])")
-
     for instruction in instructions:
-        if (match_ := remove.match(instruction)) is not None:
+        if (match_ := REMOVE_REGEX.match(instruction)) is not None:
             label = match_[1]
             box = boxes[custom_hash(label)]
             box.pop(label, None)
-        elif (match_ := assign.match(instruction)) is not None:
+        elif (match_ := ASSIGN_REGEX.match(instruction)) is not None:
             label, focal_length = match_.groups()
             box = boxes[custom_hash(label)]
             box[label] = int(focal_length)
         else:
             assert False
 
-    power = 0
-    for box_index, box in boxes.items():
-        for lens_index, focal_length in enumerate(box.values()):
-            power += (box_index + 1) * (lens_index + 1) * focal_length
-    return power
+    return calculate_power(boxes)
 
 
 def test():
