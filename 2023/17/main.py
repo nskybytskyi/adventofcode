f446a9eff272376e4903324b2c303fbca4f50dfe
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-12-29 12:53:40 +0200

    chore(refactor): 2023/17

--- 2023/17/main.py
+++ 2023/17/main.py
@@ -1,15 +1,75 @@
 #!/usr/bin/env python3
 """Clumsy Crucible"""
 import collections
+import functools
 import heapq
+from typing import Iterable
 
 
-def read_and_parse(filename: str) -> list[list[int]]:
+__Node = collections.namedtuple("Node", ["location", "direction", "count"])
+
+
+@functools.total_ordering
+class Node(__Node):
+    """fancy named tuple"""
+
+    def key(self) -> tuple[int, int, int, int, int]:
+        return (
+            self.location.real,
+            self.location.imag,
+            self.direction.real,
+            self.direction.imag,
+            self.count,
+        )
+
+    def __lt__(self, other) -> bool:
+        return self.key() < other.key()
+
+    def __eq__(self, other) -> bool:
+        return self.key() == other.key()
+
+    def __hash__(self) -> int:
+        return hash(self.key())
+
+
+class Grid:
+    """2d grid with complex indexing"""
+
+    def __init__(self, raw: list[str]):
+        self.raw = [list(map(int, line)) for line in raw]
+
+    def shape(self) -> tuple[int, int]:
+        return len(self.raw), len(self.raw[0])
+
+    def set_bounds(self, lower_bound: int, upper_bound: int):
+        self.lower_bound, self.upper_bound = lower_bound, upper_bound
+
+    def __contains__(self, cell: complex) -> bool:
+        row, col = int(cell.real), int(cell.imag)
+        return 0 <= row < len(self.raw) and 0 <= col < len(self.raw[0])
+
+    def __getitem__(self, cell: complex) -> int:
+        return self.raw[int(cell.real)][int(cell.imag)]
+
+    def get_neighbors(self, node: Node) -> Iterable[tuple[Node, int]]:
+        if self.lower_bound <= node.count:
+            for new_direction in 1j * node.direction, -1j * node.direction:
+                new_node = Node(node.location + new_direction, new_direction, 1)
+                if new_node.location in self:
+                    yield new_node, self[new_node.location]
+
+        if node.count < self.upper_bound:
+            node = Node(node.location + node.direction, node.direction, node.count + 1)
+            if node.location in self:
+                yield node, self[node.location]
+
+
+def read_and_parse(filename: str) -> Grid:
     with open(filename, "r", encoding="utf-8") as file:
-        return [list(map(int, line)) for line in file.read().splitlines()]
+        return Grid(file.read().splitlines())
 
 
-def dijkstra(sources, get_neighbors):
+def dijkstra(grid: Grid, sources: list[Node]):
     distance = collections.defaultdict(lambda: 10**9)
     heap = []
 
@@ -22,7 +82,7 @@ def dijkstra(sources, get_neighbors):
         if node_distance != distance[node]:
             continue
 
-        for next_node, edge_length in get_neighbors(node):
+        for next_node, edge_length in grid.get_neighbors(node):
             if distance[next_node] > distance[node] + edge_length:
                 distance[next_node] = distance[node] + edge_length
                 heapq.heappush(heap, (distance[next_node], next_node))
@@ -30,78 +90,45 @@ def dijkstra(sources, get_neighbors):
     return distance
 
 
-def solve_part_one(heatloss: list[list[int]]) -> int:
-    rows, cols = len(heatloss), len(heatloss[0])
-
-    def get_neighbors(node):
-        row, col, dir_row, dir_col, cnt = node
-
-        if cnt < 3:
-            next_row, next_col = row + dir_row, col + dir_col
-            next_node = next_row, next_col, dir_row, dir_col, cnt + 1
-            if 0 <= next_row < rows and 0 <= next_col < cols:
-                yield next_node, heatloss[next_row][next_col]
-
-        for next_dir_row, next_dir_col in (dir_col, -dir_row), (-dir_col, dir_row):
-            next_row, next_col = row + next_dir_row, col + next_dir_col
-            next_node = next_row, next_col, next_dir_row, next_dir_col, 1
-            if 0 <= next_row < rows and 0 <= next_col < cols:
-                yield next_node, heatloss[next_row][next_col]
-
-    distance = dijkstra(((0, 0, 0, 1, 0), (0, 0, 1, 0, 0)), get_neighbors)
-
+def solve_part_one(grid: Grid) -> int:
+    grid.set_bounds(0, 3)
+    distance = dijkstra(grid, [Node(0, 1j, 0), Node(0, 1, 0)])
+    rows, cols = grid.shape()
     return min(
         node_distance
-        for (row, col, *_), node_distance in distance.items()
-        if row == rows - 1 and col == cols - 1
+        for node, node_distance in distance.items()
+        if node.location == rows - 1 + (cols - 1) * 1j
     )
 
 
-def solve_part_two(heatloss: list[list[int]]) -> int:
-    rows, cols = len(heatloss), len(heatloss[0])
-
-    def get_neighbors(node):
-        row, col, dir_row, dir_col, cnt = node
-
-        if cnt < 10:
-            next_row, next_col = row + dir_row, col + dir_col
-            next_node = next_row, next_col, dir_row, dir_col, cnt + 1
-            if 0 <= next_row < rows and 0 <= next_col < cols:
-                yield next_node, heatloss[next_row][next_col]
-
-        if cnt >= 4:
-            for next_dir_row, next_dir_col in (dir_col, -dir_row), (-dir_col, dir_row):
-                next_row, next_col = row + next_dir_row, col + next_dir_col
-                next_node = next_row, next_col, next_dir_row, next_dir_col, 1
-                if 0 <= next_row < rows and 0 <= next_col < cols:
-                    yield next_node, heatloss[next_row][next_col]
-
-    distance = dijkstra(((0, 0, 0, 1, 0), (0, 0, 1, 0, 0)), get_neighbors)
-
+def solve_part_two(grid: Grid) -> int:
+    grid.set_bounds(4, 10)
+    distance = dijkstra(grid, [Node(0, 1j, 0), Node(0, 1, 0)])
+    rows, cols = grid.shape()
     return min(
         node_distance
-        for (row, col, *_, cnt), node_distance in distance.items()
-        if row == rows - 1 and col == cols - 1 and cnt >= 4
+        for node, node_distance in distance.items()
+        if node.location == rows - 1 + (cols - 1) * 1j and node.count >= 4
     )
 
 
 def test():
-    heatloss = read_and_parse("example-1.txt")
-    part_one_answer = solve_part_one(heatloss)
+    grid = read_and_parse("example-1.txt")
+    part_one_answer = solve_part_one(grid)
     assert part_one_answer == 102
-    part_two_answer = solve_part_two(heatloss)
+    part_two_answer = solve_part_two(grid)
     assert part_two_answer == 94
 
-    heatloss = read_and_parse("example-2.txt")
-    part_two_answer = solve_part_two(heatloss)
+    grid = read_and_parse("example-2.txt")
+    part_two_answer = solve_part_two(grid)
     assert part_two_answer == 71
 
 
 def main():
-    heatloss = read_and_parse("input.txt")
-    part_one_answer = solve_part_one(heatloss)
+    grid = read_and_parse("input.txt")
+    part_one_answer = solve_part_one(grid)
     print(f"Part One: {part_one_answer}")
-    part_two_answer = solve_part_two(heatloss)
+    part_two_answer = solve_part_two(grid)
     print(f"Part Two: {part_two_answer}")
 
 
