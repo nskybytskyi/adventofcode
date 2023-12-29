6aa24391d533e671d5cd3f3c5db18937259db423
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-12-29 12:32:43 +0200

    chore(refactor): 2023/16

--- 2023/16/main.py
+++ 2023/16/main.py
@@ -1,86 +1,90 @@
 #!/usr/bin/env python3
-"""The contraption Will Be Lava"""
+"""The Floor Will Be Lava"""
 import collections
+from typing import Iterable
 
 
-def breadth_first_search(
-    contraption: list[str], initial_location: complex, initial_direction: complex
-) -> int:
-    seen = set()
-    queue = collections.deque([(initial_location, initial_direction)])
+class Grid:
+    """2d grid with complex indexing"""
+
+    def __init__(self, raw: list[str]):
+        self.raw = raw[:]
+
+    def shape(self) -> tuple[int, int]:
+        return len(self.raw), len(self.raw[0])
+
+    def __contains__(self, cell: complex) -> bool:
+        row, col = int(cell.real), int(cell.imag)
+        return 0 <= row < len(self.raw) and 0 <= col < len(self.raw[0])
+
+    def __getitem__(self, cell: complex) -> str:
+        return self.raw[int(cell.real)][int(cell.imag)]
+
+    def find_neighbors(
+        self, location: complex, direction: complex
+    ) -> Iterable[tuple[complex, complex]]:
+        if self[location] == "|" and direction.imag:
+            yield location + 1, 1
+            yield location - 1, -1
+        elif self[location] == "-" and direction.real:
+            yield location + 1j, 1j
+            yield location - 1j, -1j
+        elif self[location] == "\\":
+            direction = direction.imag + direction.real * 1j
+            yield location + direction, direction
+        elif self[location] == "/":
+            direction = direction.imag + direction.real * 1j
+            yield location - direction, -direction
+        else:
+            yield location + direction, direction
+
+
+def breadth_first_search(grid: Grid, start: tuple[complex, complex]) -> int:
+    seen = {start}
+    queue = collections.deque([start])
 
     while queue:
-        location, direction = queue.popleft()
-
-        if (
-            location.real < 0
-            or location.real >= len(contraption)
-            or location.imag < 0
-            or location.imag >= len(contraption[0])
-            or (location, direction) in seen
-        ):
-            continue
-
-        seen.add((location, direction))
-        cell = contraption[int(location.real)][int(location.imag)]
-
-        if cell == "|" and direction.imag:
-            queue.append((location + 1, 1))
-            queue.append((location - 1, -1))
-        elif cell == "-" and direction.real:
-            queue.append((location + 1j, 1j))
-            queue.append((location - 1j, -1j))
-        elif cell == "\\":
-            direction = direction.imag + direction.real * 1j
-            queue.append((location + direction, direction))
-        elif cell == "/":
-            direction = direction.imag + direction.real * 1j
-            queue.append((location - direction, -direction))
-        else:
-            queue.append((location + direction, direction))
+        for node in grid.find_neighbors(*queue.popleft()):
+            if node[0] in grid and node not in seen:
+                seen.add(node)
+                queue.append(node)
 
     return len({location for location, _ in seen})
 
 
-def read_and_parse(filename: str) -> list[str]:
+def read_and_parse(filename: str) -> Grid:
     with open(filename, "r", encoding="utf-8") as file:
-        return file.read().splitlines()
+        return Grid(file.read().splitlines())
 
 
-def solve_part_one(contraption: list[str]) -> int:
-    return breadth_first_search(contraption, 0, 1j)
+def solve_part_one(grid: Grid) -> int:
+    return breadth_first_search(grid, (0, 1j))
 
 
-def solve_part_two(contraption: list[str]) -> int:
-    rows, cols = len(contraption), len(contraption[0])
-
-    ans = 0
-    for row in range(rows):
-        val = breadth_first_search(contraption, row, 1j)
-        ans = max(ans, val)
-        val = breadth_first_search(contraption, row + (cols - 1) * 1j, -1j)
-        ans = max(ans, val)
-    for col in range(cols):
-        val = breadth_first_search(contraption, col * 1j, 1)
-        ans = max(ans, val)
-        val = breadth_first_search(contraption, rows - 1 + col * 1j, -1)
-        ans = max(ans, val)
-    return ans
+def solve_part_two(grid: Grid) -> int:
+    rows, cols = grid.shape()
+    candidates = (
+        [(row, 1j) for row in range(rows)]
+        + [(row + (cols - 1) * 1j, -1j) for row in range(rows)]
+        + [(col * 1j, 1) for col in range(cols)]
+        + [(rows - 1 + col * 1j, -1) for col in range(cols)]
+    )
+    return max(breadth_first_search(grid, candidate) for candidate in candidates)
 
 
 def test():
-    contraption = read_and_parse("example.txt")
-    part_one_answer = solve_part_one(contraption)
+    grid = read_and_parse("example.txt")
+    part_one_answer = solve_part_one(grid)
     assert part_one_answer == 46
-    part_two_answer = solve_part_two(contraption)
+    part_two_answer = solve_part_two(grid)
     assert part_two_answer == 51
 
 
 def main():
-    contraption = read_and_parse("input.txt")
-    part_one_answer = solve_part_one(contraption)
+    grid = read_and_parse("input.txt")
+    part_one_answer = solve_part_one(grid)
     print(f"Part One: {part_one_answer}")
-    part_two_answer = solve_part_two(contraption)
+    part_two_answer = solve_part_two(grid)
     print(f"Part Two: {part_two_answer}")
 
 
